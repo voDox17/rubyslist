978ae2da7b3d9f4b8c05967fa5e816f05dcc0ef6
Rob Williams <robdwilliams85@gmail.com>
2015-03-23 21:30:03 +0000

    seeded database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :categories do
+    resources :subcategories
+  end
+
   root 'categories#index'
+  
   match '/help', to: 'pages#help', via: :get
   match '/scams', to: 'pages#scams', via: :get
   match '/safety', to: 'pages#safety', via: :get
